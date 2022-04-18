7015dcbd84dd066e89c028394bd2532b33f71e92
Krupnov <kruevg2@gmail.com>
2022-04-18 09:46:58 +0300

    Выполнены задания восьмого урока

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { ChatScreen } from './screens/ChatScreen';
+import { Home } from './screens/Home';
+import { Profile } from './screens/Profile'
+import { ChatList } from './components/ChatList/ChatList';
+import { Provider } from "react-redux";
+import { store, persistor } from "./store";
+import { PersistGate } from 'redux-persist/integration/react';
+import { Articles } from './screens/Articles/Articles';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/chat">Chat</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/articles">Articles</Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/chat" element={<ChatList />}>
+              <Route path=":id" element={<ChatScreen />} />
+            </Route>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/articles" element={<Articles />} />
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  )
+}
+
+export default App;
